42e7d1d2b6fc469b49a5a53a76f55fc5992ad345
Ridah Shoorbajee <Ridah.Shoorbajee@gmail.com>
2024-06-20 16:11:36 -0400

    New generator

--- RandomGenerator.py
+++ RandomGenerator.py
@@ -1,2 +1,9 @@
-print ("hello world")
-print ("hello world")
+import random
+import string
+
+def random_password_generator(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(length))
+
+# Example usage
+print(random_password_generator(12))
